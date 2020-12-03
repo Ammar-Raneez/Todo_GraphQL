9791063c4d7299cabed77d0049ba2c9ab3f14852
Ammar <--add>
2020-12-03 22:39:12 +0530

    getting started, using Apollo to log the todos we stored in the table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
 
+//list todos
+//add todps
+//toggle todos
+//delete todos
 function App() {
 	return (
 		<div className="app">
--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "https://pumped-barnacle-20.hasura.app/v1/graphql",
+  cache: new InMemoryCache()
+})
+
+//executed as a promise as an http request, does the get request
+//getTodos simply returns all the fields of our todos table we created in hasura
+// client.query({
+//   query: gql `
+//     query getTodos {
+//         todos {
+//           done
+//           id
+//           text
+//         }
+//       }
+//   `
+// })
+// .then(data => console.log(data))
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
