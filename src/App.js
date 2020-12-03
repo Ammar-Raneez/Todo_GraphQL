a602323029d4c7f90b7633c83639309afb5fd984
Ammar <--add>
2020-12-03 22:54:29 +0530

    tachyon styling

--- src/App.js
+++ src/App.js
@@ -23,20 +23,24 @@ function App() {
 	if(error) return <div>Error fetching todos.</div>
 
 	return (
-		<div className="app">
-			<h1>GraphQL checklist</h1>
+		<div className="vh-100 code flex flex-column items-center bg-purple white pa3 f1-1">
+			<h1 className="f2-1">GraphQL checklist {" "}
+				<span role="img" aria-label="checkmark">✔</span>
+			</h1>
 			{/* todo form */}
-			<form>
-				<input type="text" placeholder="Write your todo" />
-				<button type="submit">Create</button>
+			<form className="mb3">
+				<input className="pa2 f4 b--dashed" type="text" placeholder="Write your todo" />
+				<button className="pa2 f4 bg-green" type="submit">Create</button>
 			</form>
 
 			{/* Todo list */}
-			<div>
+			<div className="flex items-center justify-center flex-column">
 				{data.todos.map(todo => (
 					<p key={todo.id}>
-						<span>{todo.text}</span>
-						<button>&times;</button>
+						<span className="pointer list pa1 f3">{todo.text}</span>
+						<button className="bg-transparent bn f4">
+							<span className="red">&times;</span>
+						</button>
 					</p>
 				))}
 			</div>
